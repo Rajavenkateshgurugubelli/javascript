8e2104b04ac13a4d325dc8e4687811e1b4f8f3ef
Rajavenkateshgurugubelli <rajavenkatesh2000.g@gmail.com>
2024-04-03 20:34:14 -0500

    variable

--- basic/javascript-comments.js
+++
@@ -1,4 +0,0 @@
- //this is a single line comment
- /* this is a multi 
-    line comment */
-    console.log('The above lines are ignored')
---
+++ basic/variable.js
@@ -0,0 +1,13 @@
+//The variables should be start with a letter (a to z) or (A to Z), underscore(_) or dollar sign ($)
+// after the first letter we can use 0 to 9 digits
+// java script is case senstive it means TOM and tom are different variables
+
+a='he is eating'
+A='she is reading'
+console.log('this a = ',A) // the answer will be 'she is reading'
+
+_name="jhon"
+
+$age=21
+
+console.log(_name,$age) // output is 'jhon 21'
